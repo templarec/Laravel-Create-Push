92bece6e717e1aaddd814d827e63c4f89ffed7a2
Lorenzo Bernini <templarec@gmail.com>
2021-05-12 01:58:50 +0200

    Windows

---
+++ Laravel-C$P_win.ps1
@@ -0,0 +1,23 @@
+﻿$user = Read-Host -Prompt 'Inserisci username github';
+$Token = Read-Host -Prompt 'Inserisci personal Token generato in GitHub';
+$repo = Read-Host -Prompt 'Nome progetto laravel da creare';
+composer create-project --prefer-dist laravel/laravel:^7.0 $repo
+cd $repo
+
+
+$headers = @{
+    Authorization="Bearer $Token"
+}
+$body = @{
+    name= $repo; 
+
+}| ConvertTo-Json;
+
+Invoke-RestMethod -Headers $headers -Method Post -Uri https://api.github.com/user/repos -Body $body
+
+git init
+git add -A
+git commit -m "init laravel"
+git remote add origin https://github.com/$user/$repo.git
+git push -u origin master
+Write-Host "Progetto laravel creato e repo '$repo' pushata";
